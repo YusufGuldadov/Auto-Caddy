73888dcf2b4b13f8f4799acf48a96b9f6afc3a59
Zachary Szakacs <bxq315@usask.ca>
2025-03-21 16:42:54 -0600

    Combined and optimized the user tracking and obstacle detection

--- Integration/integration/motor_test.ino
+++ Integration/integration/motor_test.ino
@@ -8,6 +8,12 @@
 int currAng = 0;
 int prevAng = currAng;
 
+const int histSize = 5;
+
+int lastTenAngles[histSize] = {90};
+int lastTenSpeeds[histSize] = {-1};
+int lastTenIndex = 0;
+
 
 // Function for setting the speed to a constant value
 void setConstantSpeed(float speedVal) {
@@ -43,32 +49,50 @@ int speedToPWM(int speed){
   return speed;
 }
 
-float getAdjustedValue(float ang, int speedValue){
+struct moveValues {
+  float s;
+  int a;
+};
+
+struct moveValues getAdjustedValue(float ang, int speedValue){
   prevAng = currAng;
   currAng = ang;
 
   int currTo90 = abs(currAng - 90);
   int prevTo90 = abs(prevAng - 90);
 
-  if((prevTo90 - currTo90) >  45){ // Caddy turning towards 90
-    return speedValue*0.6;
+  if((prevTo90 - currTo90) >  30){ // Caddy turning towards 90
+    return {speedValue*0.6, ang};
   }
 
-  return speedValue;
+
+  // if(speedValue > 0){
+  //   for(auto an : lastTenAngles){
+  //     if(an == 90){
+  //       return {speedValue, 180 - currAng};
+  //     }
+  //   }
+  // }
+
+  return {speedValue, ang};
 }
 
 // MotionController controller(0.2, 0.5, 0.05);
 
 
 void moveCaddy(float speedValue, int ang) {
-  Serial.println("Angle: " + String(ang) + ", Speed: " + String(speedValue));
+  // Serial.println("Angle: " + String(ang) + ", Speed: " + String(speedValue));
   // Serial.println("," + String(speedValue) + "," + String(ang));
 
 
 
 
-  auto adjustedSpeed = getAdjustedValue(ang, speedValue);
-  auto adjustedAngle = ang; 
+  struct moveValues values = getAdjustedValue(ang, speedValue);
+
+  float adjustedSpeed = values.s;
+  int adjustedAngle = values.a;
+
+  // auto adjustedAngle = ang; 
   // auto adjustedSpeed = speedValue;
 
   float speedPWM = 41 * adjustedSpeed;    // max value of 246 ( < 250 )
@@ -76,6 +100,23 @@ void moveCaddy(float speedValue, int ang) {
   // float decrement = abs(cos(angleRad)) * speedPWM;   // This is how much one wheel will slow down to turn
   // float decrement = 1/4 * (angleRad - M_PI/2)*(angleRad - M_PI/2);
   float decrement = ((-1.0/12000.0)*(adjustedAngle-90)*(adjustedAngle-90)) + 1;
+
+  lastTenAngles[lastTenIndex] = ang;
+  lastTenSpeeds[lastTenIndex] = speedValue;
+
+  lastTenIndex++;
+  lastTenIndex = lastTenIndex % histSize;
+
+
+  // if(adjustedSpeed > 0){
+  //   for(auto sp : lastTenSpeeds){
+  //     if(sp == 0){
+  //       adjustedSpeed = 4;
+  //     }
+  //   }
+  // }
+
+
 
   if (adjustedSpeed == 0) {
     setMotorSpeeds(0, 0);
